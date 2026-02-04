57671c9b96722f03098caf7fdd0d40126f1df2a6
isaacurba <kpomassiurban@gmail.com>
2026-02-04 16:29:26 +0100

    feat: add NavBar component for navigation

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  return (
+    <nav className='w-full py-4 px-4 sm:px-6 lg:px-16 bg-card/50 backdrop-blur-sm sticky top-0 z-50'>
+        <div className='w-full lg:max-w-7xl mx-auto flex items-center justify-between'>
+            <Link to="/" className="font-bold text-2xl">Products</Link>
+
+            <Link to="/create">
+                <button className="bg-blue-500 text-white rounded-sm px-4 py-2">
+                    + Create Product
+                </button>
+            </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar;
