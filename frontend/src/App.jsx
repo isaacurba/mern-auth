3a5f205a4ce45e54b7095e1580c0d9c7f4c1e5a4
isaacurba <kpomassiurban@gmail.com>
2026-02-04 16:29:41 +0100

    feat: implement routing and navigation in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/createPage"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <>
-
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/create" element={<CreatePage />} />
+    </Routes>
     </>
   )
 }
