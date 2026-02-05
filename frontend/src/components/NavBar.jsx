61e9061481957c2d8eb3fd289de54c986e521945
isaacurba <kpomassiurban@gmail.com>
2026-02-05 23:47:27 +0100

    done with navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
   return (
     <nav className='w-full py-4 px-4 sm:px-6 lg:px-16 bg-card/50 backdrop-blur-sm sticky top-0 z-50'>
-        <div className='w-full lg:max-w-5xl mx-auto flex items-center justify-between'>
+        <div className='w-full lg:max-w-5xl mx-auto flex items-center justify-between border-b border-gray-200 p-2'>
             <Link to="/" className="font-bold text-xl">Products</Link>
 
             <Link to="/create">
